2c54522fa5de076de8f31f11e500c2f57a88faae
phuthinhnguyen1101 <phuthinhnguyen1101@gmail.com>
2023-05-06 14:06:50 +0700

    sdf

--- src/index.js
+++ src/index.js
@@ -4,13 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const profilecard = (
+  <div className="container">
+    <div className="card">
+      <div className="card--header" />
+      <img
+        className="avatar"
+        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt="avatar"
+      />
+      <div className="card--body">
+        <div>
+          <p className="text-header">Ruma Khan</p>
+          <p className="text-sub">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry
+          </p>
+          <button className="btn third">FOLLOW</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+ReactDOM.createRoot(document.getElementById("root")).render(profilecard);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
